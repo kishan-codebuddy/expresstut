b6e8719e37213fda7f47e4c012bce449b1bd864a
kishan.maharana <kishan@codebuddy.co>
2025-01-03 11:19:01 +0530

    added inficial implementation

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
+require('dotenv').config();
 const path = require('path')
 const bookRouter = require('./routes/book.js');
 const authorRouter = require('./routes/author.js');
 const genreRouter = require('./routes/genre.js');
 const {connectMongo} = require("./db_connection.js");
 const app = express()
-const port = 3000
-// connectMongo();
+
+const port = process.env.PORT;
+
+
 
 app.use(express.json());
 app.use("/books",bookRouter);
@@ -15,4 +18,4 @@ app.use("/genres",genreRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
